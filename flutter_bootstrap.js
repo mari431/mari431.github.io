4aab2f5c903480855e9c3d1891c33ab4efdf3d69
Marimuthu Kannayiram <51232711+mari431@users.noreply.github.com>
2024-08-30 10:39:27 +0530

    sample work added

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"235db911ba279722f5e685f38b0ed30fa7e857
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2905164426"
+    serviceWorkerVersion: "3688625824"
   }
 });
